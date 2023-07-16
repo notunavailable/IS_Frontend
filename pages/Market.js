0debb4593742f828b7bf2990269dbf30d650e136
notunavailable <77205554+notunavailable@users.noreply.github.com>
2023-07-16 02:49:55 -0700

    Fixed stuff for vercel

--- pages/Market.js
+++ pages/Market.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import styles from '../styles/Market.module.css';
+import styles from '../styles/market.module.css';
 
 export default function Market() {
     const products = [
