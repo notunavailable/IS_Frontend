5ebb8c77e19c052dcdf3b585d157bccaa9609d53
notunavailable <77205554+notunavailable@users.noreply.github.com>
2023-07-19 15:53:04 -0700

    Made custom 404 page.

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+const page404 = () => {
+    return(
+        <div>
+            <h2>We're sorry, the page you are looking for doesn't exist. `\_o_/`</h2>
+        </div>
+    )
+}
+
+export default page404;
