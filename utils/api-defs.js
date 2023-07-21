c868b194f88fc31f03901059a4e53f16d6f67119
notunavailable <77205554+notunavailable@users.noreply.github.com>
2023-07-20 17:25:31 -0700

    Properly fixed serverless function

--- utils/api-defs.js
+++ utils/api-defs.js
@@ -17,7 +17,7 @@ export const USER_PRACTICE = ({id}) => `${ORIGIN}/api/v1/user/practice/${id}`
 export const USER_ADDSKILL = ({id}) => `${ORIGIN}/api/v1/user/addSkill/${id}`
 export const USER_REMOVESKILL = ({id}) => `${ORIGIN}/api/v1/user/status/${id}`
 
-export const SKILL_GET_ONE = () => `${ORIGIN}/api/v1/user/skill/${id}`
+export const SKILL_GET_ONE = ({id}) => `${ORIGIN}/api/v1/user/skill/${id}`
 export const SKILL_GET = () => `${ORIGIN}/api/v1/user/skill/`
 export const SKILL_PUT = ({id}) => `${ORIGIN}/api/v1/user/skill/edit/${id}`
 export const SKILL_DELETE = ({id}) => `${ORIGIN}/api/v1/user/skill/delete/${id}`
